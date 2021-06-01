c5ebe3c05d344803ad9a6995460f81af5840540c
ashmichheda <ashmichheda@gmail.com>
2021-05-31 19:15:29 -0700

    added different for loops

--- app.js
+++ app.js
@@ -12,3 +12,20 @@ for (var x = 0; x <= 5; x++) {
     var counter = x;
 }
 console.log(counter);
+// Here in the above for loop the counter variable is accessible outside of the for-loop which
+// shouldn't be the case. But the JS var keyword does it.
+// Hence in order to avoid this weird behavior, one should use `let` keyword
+// for-in loops
+var array = [
+    "Pick up dry cleaning",
+    "Clean Batcave",
+    "Save Gotham"
+];
+for (var index in array) {
+    var value = array[index];
+    console.log(value + " : " + index);
+}
+for (var _i = 0, array_1 = array; _i < array_1.length; _i++) {
+    value = array_1[_i];
+    console.log("" + value);
+}
--- app.ts
+++ app.ts
@@ -17,3 +17,23 @@ for(var x = 0; x <= 5; x++) {
 }
 
 console.log(counter)
+
+// Here in the above for loop the counter variable is accessible outside of the for-loop which
+// shouldn't be the case. But the JS var keyword does it.
+// Hence in order to avoid this weird behavior, one should use `let` keyword
+
+// for-in loops
+var array = [
+    "Pick up dry cleaning",
+    "Clean Batcave",
+    "Save Gotham"
+];
+
+for(var index in array) {
+    var value = array[index];
+    console.log(`${value} : ${index}`)
+}
+
+for(value of array){
+    console.log(`${value}`)
+}
